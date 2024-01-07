16c791d46104f6b313583ef7f5eea6ec2c2cac6e
wijdane <wijdaneaassouss@gmail.com>
2024-01-07 13:24:08 +0100

    Product Search Feature

--- product/urls.py
+++ product/urls.py
@@ -2,7 +2,9 @@ from django.urls import path , include
 from . import views
 
 urlpatterns = [
-    path('', views.home_view),
+    # path('', views.home_view),
+    path('',views.search_product, name='home'),
+
     ]
 
 
--- product/views.py
+++ product/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
-from . models import Product
+
+
+# views.py
+# views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def search_product(request):
+    product = None
+
+    if 'barcode' in request.GET:
+        barcode = request.GET['barcode']
+        try:
+            # Attempt to find the product by barcode
+            product = Product.objects.get(barcode=barcode)
+        except Product.DoesNotExist:
+            # Product not found
+            pass
+
+    return render(request, 'home.html', {'product': product})
 
 
 
@@ -7,11 +25,13 @@ from . models import Product
 
 
 
-def home_view(request):
-    # Retrieve products from both concrete model tables
-    products = Product.objects.all()
-    context = {'products': products}
-    # return render(request, 'home.html', context)
-    return render(request, 'home.html',context)
 
+# from django.shortcuts import render
+# from . models import Product
 
+# def home_view(request):
+#     # Retrieve products from both concrete model tables
+#     products = Product.objects.all()
+#     context = {'products': products}
+#     # return render(request, 'home.html', context)
+#     return render(request, 'home.html',context)
